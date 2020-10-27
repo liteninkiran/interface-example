25b9d52c5967e88d3384be1fea8d63f59e314e92
Kiran Anand <liteninkiran@hotmail.com>
2020-10-27 20:44:34 +0000

    Add project files.

---
+++ interface-example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace interface_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
