80eb41c483f737569d1d3bf35bf7ecde1a7f4651
Kiran Anand <liteninkiran@hotmail.com>
2020-10-27 20:54:53 +0000

    Initial commit

---
+++ interface-example/Example-1.cs
@@ -0,0 +1,33 @@
+﻿// C# program to demonstrate working of  
+// interface 
+using System;
+
+// A simple interface 
+interface inter1
+{
+    // method having only declaration  
+    // not definition 
+    void display();
+}
+
+// A class that implements interface. 
+class testClass : inter1
+{
+
+    // providing the body part of function 
+    public void display()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    // Main Method 
+    public static void Main_Method()
+    {
+
+        // Creating object 
+        testClass t = new testClass();
+
+        // calling method 
+        t.display();
+    }
+}
---
+++ interface-example/Main-Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace interface_example
+{
+    class Main_Class
+    {
+        public static void Main(String[] args)
+        {
+            testClass.Main_Method();
+            GFG.Main_Method();
+        }
+    }
+}
